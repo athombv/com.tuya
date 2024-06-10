14c72fafa356a8844c7584edda25ab279ced1995
Emile Nijssen <emile@emilenijssen.nl>
2024-06-10 17:33:49 +0200

    v0.4.0

--- drivers/other/driver.js
+++ drivers/other/driver.js
@@ -10,19 +10,92 @@ module.exports = class TuyaOAuth2DriverOther extends TuyaOAuth2Driver {
     this.homey.flow
       .getActionCard('send_command_string')
       .registerRunListener(async ({ device, code, value }) => {
+        if (typeof code === 'object') code = code.id;
+
         await device.sendCommand({ code, value });
+      })
+      .registerArgumentAutocompleteListener('code', async (query, args) => {
+        const status = await args.device.getStatus();
+        return status
+          .filter(({ value }) => {
+            return typeof value === 'string' && !value.startsWith('{');
+          })
+          .filter(({ code }) => {
+            return code.toLowerCase().includes(query.toLowerCase());
+          })
+          .map(({ code }) => ({
+            id: code,
+            title: code,
+          }));
       });
 
     this.homey.flow
       .getActionCard('send_command_number')
       .registerRunListener(async ({ device, code, value }) => {
+        if (typeof code === 'object') code = code.id;
+
         await device.sendCommand({ code, value });
+      })
+      .registerArgumentAutocompleteListener('code', async (query, args) => {
+        const status = await args.device.getStatus();
+        return status
+          .filter(({ value }) => {
+            return typeof value === 'number';
+          })
+          .filter(({ code }) => {
+            return code.toLowerCase().includes(query.toLowerCase());
+          })
+          .map(({ code }) => ({
+            id: code,
+            title: code,
+          }));
       });
 
     this.homey.flow
       .getActionCard('send_command_boolean')
       .registerRunListener(async ({ device, code, value }) => {
+        if (typeof code === 'object') code = code.id;
+
         await device.sendCommand({ code, value });
+      })
+      .registerArgumentAutocompleteListener('code', async (query, args) => {
+        const status = await args.device.getStatus();
+        return status
+          .filter(({ value }) => {
+            return typeof value === 'boolean';
+          })
+          .filter(({ code }) => {
+            return code.toLowerCase().includes(query.toLowerCase());
+          })
+          .map(({ code }) => ({
+            id: code,
+            title: code,
+          }));
+      });
+
+    this.homey.flow
+      .getActionCard('send_command_json')
+      .registerRunListener(async ({ device, code, value }) => {
+        if (typeof code === 'object') code = code.id;
+
+        await device.sendCommand({
+          code,
+          value: JSON.parse(value),
+        });
+      })
+      .registerArgumentAutocompleteListener('code', async (query, args) => {
+        const status = await args.device.getStatus();
+        return status
+          .filter(({ value }) => {
+            return typeof value === 'string' && value.startsWith('{');
+          })
+          .filter(({ code }) => {
+            return code.toLowerCase().includes(query.toLowerCase());
+          })
+          .map(({ code }) => ({
+            id: code,
+            title: code,
+          }));
       });
   }
 
