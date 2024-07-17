4cabd1c6f6cb7901ebe9a94e1fb089f2994cdbe6
Joost Loohuis <github@loohuis.dev>
2024-07-17 14:33:48 +0200

    Add Tuya device properties to settings of "other" device

--- drivers/other/driver.js
+++ drivers/other/driver.js
@@ -1,11 +1,26 @@
-'use strict';
+"use strict";
 
-const TuyaOAuth2Driver = require('../../lib/TuyaOAuth2Driver');
+const TuyaOAuth2Driver = require("../../lib/TuyaOAuth2Driver");
 
 module.exports = class TuyaOAuth2DriverOther extends TuyaOAuth2Driver {
-
   onTuyaPairListDeviceFilter() {
     return true; // Accept any device
   }
 
+  onTuyaPairListDeviceProperties(device, specifications) {
+    const props = super.onTuyaPairListDeviceProperties(device);
+
+    const combinedSpecification = {
+      device: device,
+      specifications: specifications,
+    };
+
+    props.settings["deviceSpecification"] = JSON.stringify(
+      combinedSpecification,
+      undefined,
+      2,
+    );
+
+    return props;
+  }
 };
