6b5195bd363685ee31c4eb331b5b9eb2f3c5ca75
Joost Loohuis <github@loohuis.dev>
2024-08-14 11:47:20 +0200

    Add prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,6 +7,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'prettier',
   ],
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'error',
@@ -14,7 +15,6 @@ module.exports = {
     '@typescript-eslint/ban-ts-comment': ['warn', {
       'ts-ignore': 'allow-with-description',
     }],
-    "semi": [2, "always"]
   },
   env: {
     node: true
