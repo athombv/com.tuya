bb98078c7f39de613a33034e5e096b880ead2d07
Joost Loohuis <github@loohuis.dev>
2024-08-30 17:34:41 +0200

    Refactor light driver to generic class

--- drivers/light/driver.ts
+++ drivers/light/driver.ts
@@ -1,5 +1,5 @@
 import { DEVICE_CATEGORIES, TUYA_PERCENTAGE_SCALING } from '../../lib/TuyaOAuth2Constants';
-import TuyaOAuth2Driver, { ListDeviceProperties } from '../../lib/TuyaOAuth2Driver';
+import { ListDeviceProperties } from '../../lib/TuyaOAuth2Driver';
 import { constIncludes } from '../../lib/TuyaOAuth2Util';
 import {
   type TuyaDeviceDataPointResponse,
@@ -9,11 +9,12 @@ import {
 import type { StandardDeviceFlowArgs, StandardFlowArgs } from '../../types/TuyaTypes';
 import type TuyaOAuth2DeviceLight from './device';
 import { LightSettingCommand, PIR_CAPABILITIES } from './TuyaLightConstants';
+import TuyaOAuth2DriverWithLight from '../../lib/TuyaOAuth2DriverWithLight';
 
 type DeviceArgs = StandardDeviceFlowArgs<TuyaOAuth2DeviceLight>;
 type FlowArgs = StandardFlowArgs<TuyaOAuth2DeviceLight>;
 
-module.exports = class TuyaOAuth2DriverLight extends TuyaOAuth2Driver {
+module.exports = class TuyaOAuth2DriverLight extends TuyaOAuth2DriverWithLight {
   TUYA_DEVICE_CATEGORIES = [
     DEVICE_CATEGORIES.LIGHTING.LIGHT,
     DEVICE_CATEGORIES.LIGHTING.CEILING_LIGHT,
@@ -89,6 +90,7 @@ module.exports = class TuyaOAuth2DriverLight extends TuyaOAuth2Driver {
     specifications?: TuyaDeviceSpecificationResponse,
     dataPoints?: TuyaDeviceDataPointResponse,
   ): ListDeviceProperties {
+    // superclass handles light capabilities, except onoff
     const props = super.onTuyaPairListDeviceProperties(device, specifications, dataPoints);
     props.store.tuya_switches = [];
 
@@ -159,31 +161,6 @@ module.exports = class TuyaOAuth2DriverLight extends TuyaOAuth2Driver {
     for (const status of device.status) {
       const tuyaCapability = status.code;
 
-      // dim
-      if (tuyaCapability === 'bright_value' || tuyaCapability === 'bright_value_v2') {
-        props.store.tuya_capabilities.push(tuyaCapability);
-        props.capabilities.push('dim');
-      }
-
-      // light temperature
-      if (tuyaCapability === 'temp_value' || tuyaCapability === 'temp_value_v2') {
-        props.store.tuya_capabilities.push(tuyaCapability);
-        props.capabilities.push('light_temperature');
-      }
-
-      // light hue and saturation
-      if (tuyaCapability === 'colour_data' || tuyaCapability === 'colour_data_v2') {
-        props.store.tuya_capabilities.push(tuyaCapability);
-        props.capabilities.push('light_hue');
-        props.capabilities.push('light_saturation');
-        props.capabilities.push('dim');
-      }
-
-      // light_mode
-      if (tuyaCapability === 'work_mode') {
-        props.store.tuya_capabilities.push(tuyaCapability);
-      }
-
       // motion alarm
       if (tuyaCapability === 'pir_state') {
         props.store.tuya_capabilities.push(tuyaCapability);
@@ -204,73 +181,6 @@ module.exports = class TuyaOAuth2DriverLight extends TuyaOAuth2Driver {
 
     // Remove duplicate capabilities
     props.capabilities = [...new Set(props.capabilities)];
-
-    // Only add light mode capability when both temperature and colour data is available
-    if (props.capabilities.includes('light_temperature') && props.capabilities.includes('light_hue')) {
-      props.capabilities.push('light_mode');
-    }
-
-    // Category Specifications
-    // The main light category has both (0,255) and (0,1000) for backwards compatibility
-    // Other categories use only (0,1000)
-    if (device.category === 'dj') {
-      props.store.tuya_brightness = { min: 25, max: 255, scale: 0, step: 1 };
-      props.store.tuya_temperature = { min: 0, max: 255, scale: 0, step: 1 };
-      props.store.tuya_colour = {
-        h: { min: 0, max: 360, scale: 0, step: 1 },
-        s: { min: 0, max: 255, scale: 0, step: 1 },
-        v: { min: 0, max: 255, scale: 0, step: 1 },
-      };
-      props.store.tuya_brightness_v2 = { min: 10, max: 1000, scale: 0, step: 1 };
-      props.store.tuya_temperature_v2 = { min: 0, max: 1000, scale: 0, step: 1 };
-      props.store.tuya_colour_v2 = {
-        h: { min: 0, max: 360, scale: 0, step: 1 },
-        s: { min: 0, max: 1000, scale: 0, step: 1 },
-        v: { min: 0, max: 1000, scale: 0, step: 1 },
-      };
-    } else {
-      props.store.tuya_brightness = { min: 10, max: 1000, scale: 0, step: 1 };
-      props.store.tuya_temperature = { min: 0, max: 1000, scale: 0, step: 1 };
-      props.store.tuya_colour = {
-        h: { min: 0, max: 360, scale: 0, step: 1 },
-        s: { min: 0, max: 1000, scale: 0, step: 1 },
-        v: { min: 0, max: 1000, scale: 0, step: 1 },
-      };
-    }
-
-    if (!specifications || !specifications.functions) {
-      return props;
-    }
-
-    // Device Specifications
-    for (const functionSpecification of specifications.functions) {
-      const tuyaCapability = functionSpecification.code;
-      const values = JSON.parse(functionSpecification.values);
-
-      if (tuyaCapability === 'bright_value') {
-        props.store.tuya_brightness = { ...props.store.tuya_brightness, ...values };
-      } else if (tuyaCapability === 'bright_value_v2') {
-        props.store.tuya_brightness_v2 = { ...props.store.tuya_brightness_v2, ...values };
-      } else if (tuyaCapability === 'temp_value') {
-        props.store.tuya_temperature = { ...props.store.tuya_temperature, ...values };
-      } else if (tuyaCapability === 'temp_value_v2') {
-        props.store.tuya_temperature_v2 = { ...props.store.tuya_temperature_v2, ...values };
-      } else if (tuyaCapability === 'colour_data') {
-        for (const index of ['h', 's', 'v']) {
-          props.store.tuya_colour[index] = {
-            ...props.store.tuya_colour[index],
-            ...values?.[index],
-          };
-        }
-      } else if (tuyaCapability === 'colour_data_v2') {
-        for (const index of ['h', 's', 'v']) {
-          props.store.tuya_colour_v2[index] = {
-            ...props.store.tuya_colour_v2[index],
-            ...values?.[index],
-          };
-        }
-      }
-    }
 
     return props;
   }
