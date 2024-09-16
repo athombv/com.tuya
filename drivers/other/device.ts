683ebe71df30017542cefc9c190faa169e9aefd9
Joost Loohuis <github@loohuis.dev>
2024-09-16 14:17:37 +0200

    Only retrieve details for 'other' device that is actually paired

--- drivers/other/device.ts
+++ drivers/other/device.ts
@@ -1,3 +1,9 @@
 import TuyaOAuth2Device from '../../lib/TuyaOAuth2Device';
+import * as OtherMigrations from '../../lib/migrations/OtherMigrations';
 
-module.exports = class TuyaOAuth2DeviceOther extends TuyaOAuth2Device {};
+module.exports = class TuyaOAuth2DeviceOther extends TuyaOAuth2Device {
+  async performMigrations(): Promise<void> {
+    await super.performMigrations();
+    await OtherMigrations.performMigrations(this);
+  }
+};
