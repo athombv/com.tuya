c47e7e7d5fb15926e3d2a2cb85e4a1895c32a5fd
Joost Loohuis <github@loohuis.dev>
2024-09-23 12:43:24 +0200

    Add dehumidifier

---
+++ drivers/dehumidifier/driver.ts
@@ -0,0 +1,102 @@
+import { DEVICE_CATEGORIES } from '../../lib/TuyaOAuth2Constants';
+import TuyaOAuth2Driver, { ListDeviceProperties } from '../../lib/TuyaOAuth2Driver';
+import {
+  type TuyaDeviceDataPointResponse,
+  TuyaDeviceResponse,
+  TuyaDeviceSpecificationResponse,
+} from '../../types/TuyaApiTypes';
+import { constIncludes, getFromMap } from '../../lib/TuyaOAuth2Util';
+import { DEHUMIDIFIER_CAPABILITIES, DEHUMIDIFIER_CAPABILITY_MAPPING } from './DehumidifierConstants';
+import { StandardFlowArgs } from '../../types/TuyaTypes';
+import dehumidifier_mode from '../../.homeycompose/capabilities/dehumidifier_mode.json';
+import target_humidity_enum from '../../.homeycompose/capabilities/target_humidity_enum.json';
+import driver_compose from './driver.compose.json';
+
+module.exports = class TuyaOAuth2DriverDehumidifier extends TuyaOAuth2Driver {
+  TUYA_DEVICE_CATEGORIES = [DEVICE_CATEGORIES.SMALL_HOME_APPLIANCES.DEFUMIDIFIER] as const;
+
+  async onInit(): Promise<void> {
+    await super.onInit();
+
+    for (const tuyaCapability of ['child_lock', 'sleep', 'defrost', 'swing'] as const) {
+      const homeyCapability = DEHUMIDIFIER_CAPABILITY_MAPPING[tuyaCapability];
+      this.homey.flow
+        .getActionCard(`dehumidifier_${homeyCapability}`)
+        .registerRunListener((args: StandardFlowArgs) =>
+          args.device.triggerCapabilityListener(homeyCapability, args.value),
+        );
+    }
+
+    for (const tuyaCapability of ['mode', 'fan_speed_enum', 'dehumidify_set_enum'] as const) {
+      const homeyCapability = DEHUMIDIFIER_CAPABILITY_MAPPING[tuyaCapability];
+      this.addEnumActionFlowHandler(homeyCapability, `dehumidifier_${homeyCapability}`);
+    }
+  }
+
+  onTuyaPairListDeviceProperties(
+    device: TuyaDeviceResponse,
+    specifications?: TuyaDeviceSpecificationResponse,
+    dataPoints?: TuyaDeviceDataPointResponse,
+  ): ListDeviceProperties {
+    const props = super.onTuyaPairListDeviceProperties(device, specifications, dataPoints);
+
+    for (const status of device.status) {
+      const tuyaCapability = status.code;
+      const homeyCapability = getFromMap(DEHUMIDIFIER_CAPABILITY_MAPPING, tuyaCapability);
+
+      if (homeyCapability) {
+        props.store.tuya_capabilities.push(tuyaCapability);
+        props.capabilities.push(homeyCapability);
+      }
+    }
+
+    // Default values, since capabilitiesOptions is not filled from the capability definitions
+    props.capabilitiesOptions['dehumidifier_mode'] = {
+      values: dehumidifier_mode.values,
+    };
+    props.capabilitiesOptions['legacy_fan_speed'] = {
+      values: driver_compose.capabilitiesOptions.legacy_fan_speed.values,
+    };
+    props.capabilitiesOptions['target_humidity_enum'] = {
+      values: target_humidity_enum.values,
+    };
+
+    if (!specifications || !specifications.status) {
+      return props;
+    }
+
+    for (const statusSpecification of specifications.status) {
+      const tuyaCapability = statusSpecification.code;
+      const values = JSON.parse(statusSpecification.values);
+      const homeyCapability = getFromMap(DEHUMIDIFIER_CAPABILITY_MAPPING, tuyaCapability);
+
+      if (constIncludes(DEHUMIDIFIER_CAPABILITIES.read_only_scaled, tuyaCapability)) {
+        if ([0, 1, 2, 3].includes(values.scale)) {
+          props.settings[`${homeyCapability}_scaling`] = `${values.scale}`;
+        } else {
+          this.error(`Unsupported ${tuyaCapability} scale:`, values.scale);
+        }
+      }
+
+      if (
+        ['mode', 'fan_speed_enum', 'dehumidify_set_enum'].includes(tuyaCapability) &&
+        homeyCapability &&
+        Array.isArray(values.range) &&
+        values.range.length > 0
+      ) {
+        props.capabilitiesOptions[homeyCapability] = {
+          values: (values.range as string[]).map(value => {
+            const title =
+              tuyaCapability === 'dehumidify_set_enum' ? `${value}%` : value.charAt(0).toUpperCase() + value.slice(1);
+            return {
+              id: value,
+              title: title,
+            };
+          }),
+        };
+      }
+    }
+
+    return props;
+  }
+};
