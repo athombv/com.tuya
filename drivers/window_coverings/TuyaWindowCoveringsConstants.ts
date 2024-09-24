f1d8ebeb87885a995af44d68b83ca3d4fb862e45
Bob van de Vijver <bob@drenso.nl>
2024-09-24 10:04:04 +0200

    Rename curtain to more generic window coverings

--- drivers/window_coverings/TuyaWindowCoveringsConstants.ts
+++ drivers/window_coverings/TuyaWindowCoveringsConstants.ts
@@ -1,6 +1,6 @@
 import TRANSLATIONS from './translations.json';
 
-export const CURTAIN_CAPABILITY_MAPPING = {
+export const WINDOW_COVERINGS_CAPABILITY_MAPPING = {
   control: 'windowcoverings_state',
   mach_operate: 'windowcoverings_state',
   position: 'windowcoverings_set',
@@ -8,19 +8,19 @@ export const CURTAIN_CAPABILITY_MAPPING = {
   percent_state: 'windowcoverings_set',
 } as const;
 
-export const CURTAIN_CAPABILITIES = {
+export const WINDOW_COVERINGS_CAPABILITIES = {
   read_write: ['control', 'position', 'mach_operate', 'percent_control'],
   setting: ['opposite', 'control_back'],
 } as const;
 
-export type HomeyCurtainSettings = {
+export type HomeyWindowCoveringsSettings = {
   inverse: boolean;
 };
 
-export type TuyaCurtainSettings = {
+export type TuyaWindowCoveringsSettings = {
   opposite: boolean; // inverse
   control_back: boolean; // inverse
   control_back_mode: 'forward' | 'back'; // inverse
 };
 
-export const CURTAIN_SETTING_LABELS = TRANSLATIONS.setting_labels;
+export const WINDOW_COVERINGS_SETTING_LABELS = TRANSLATIONS.setting_labels;
